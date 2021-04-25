d4d7c696d0636b5a07d21dc2a6d41fe67ab61788
Vitor Oliveira <vbrazo@gmail.com>
2021-04-25 15:26:55 -0700

    Add invalid parameter test for auth

--- test/blog_api/auth_test.exs
+++ test/blog_api/auth_test.exs
@@ -12,12 +12,21 @@ defmodule BlogApi.Accounts.AuthTest do
     image: "some image"
   }
 
-  test "register/1 creates a user" do
-    Auth.register(@user_create_attrs)
+  describe "given valid parameters" do
+    test "register/1 creates a user" do
+      Auth.register(@user_create_attrs)
+    end
   end
 
-  test "register/1 returns error if email is used already" do
-    Auth.register(@user_create_attrs)
-    assert {:error, _} = Auth.register(@user_create_attrs)
+  describe "given invalid parameters" do
+    test "register/1 does not create a user" do
+      Auth.register(%{})
+      assert {:error, _} = Auth.register(%{})
+    end
+
+    test "register/1 returns error if email is used already" do
+      Auth.register(@user_create_attrs)
+      assert {:error, _} = Auth.register(@user_create_attrs)
+    end
   end
 end
